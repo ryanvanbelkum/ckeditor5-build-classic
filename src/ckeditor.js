2479ee00daaa4d26af4b75fafbceab4651aa18c4
Ryan Vanbelkum <rvanbelkum@grubhub.com>
2020-03-22 08:38:53 -0500

    switching to simple upload adapter

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -28,7 +28,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleUploadAdapter';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +56,7 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	Base64UploadAdapter
+	SimpleUploadAdapter
 ];
 
 // Editor configuration.
